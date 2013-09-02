08b95b20eba77b17e108380c4b71ac99eac85349
Xiaodong <zhang6078349@163.com>
2013-09-02 00:36:14 -0400

    added part of the functionality

--- src/testtest/StringReorderDistanceApart.java
+++ src/testtest/StringReorderDistanceApart.java
@@ -5,30 +5,63 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Set;
 
 public class StringReorderDistanceApart {
 
-	public static String reorder(String str, int dist) {
+	// use a greedy strategy
+	static String reorder(String str, int dist) {
+
+		Map<Character, Integer> contentMap = sortCharactors(str);
+
+		FixedSizeQueue<Character> window = new FixedSizeQueue<Character>(dist);
+
+		StringBuilder sb = new StringBuilder();
+
+		while (!contentMap.isEmpty()) {
+
+			Set<Character> keys = contentMap.keySet();
+
+			for (Character key : keys) {
+
+				if (contentMap.get(key) == 0) {
+
+					contentMap.remove(key);
+
+				} else {
+
+					if (!window.contains(key)) {
+
+						sb.append(key);
+						window.addLast(key);
+						contentMap.put(key, contentMap.get(key) - 1);
+					}
+				}
+			}
+		}
+
+		return sb.toString();
+	}
+
+	static Map<Character, Integer> sortCharactors(String str) {
 
 		Map<Character, Integer> contentMap = new HashMap<Character, Integer>();
 
 		for (int i = 0; i < str.length(); i++) {
-			
 			char c = str.charAt(i);
-			
+
 			if (contentMap.keySet().contains(c)) {
-				
 				contentMap.put(c, contentMap.get(c) + 1);
 			} else {
-			
 				contentMap.put(c, 1);
 			}
 		}
-		
-		System.out.println(contentMap);
+
+		System.out.println("before sorted: " + contentMap);
 
 		List<Entry<Character, Integer>> entryList = new ArrayList<Entry<Character, Integer>>(
 				contentMap.entrySet());
@@ -36,20 +69,22 @@ public class StringReorderDistanceApart {
 		Collections.sort(entryList, new ValueComparator());
 
 		contentMap = new LinkedHashMap<Character, Integer>();
-		
-		for(Entry<Character, Integer> entry : entryList)
-			contentMap.put(entry.getKey(), entry.getValue());
-		
-		System.out.println(contentMap);
-		
-		// LinkedList<Character> window = new LinkedList<Character>();
 
-		return null;
+		for (Entry<Character, Integer> entry : entryList) {
+			contentMap.put(entry.getKey(), entry.getValue());
+		}
+
+		System.out.println("after sorted: " + contentMap);
+
+		return contentMap;
 	}
 
 	public static void main(String[] args) {
 
-		reorder("ddddaaaaaabbbbbccc", 2);
+		final int DISTANCE = 3;
+		final String INPUT = "ddddaaaaaabbbbbccc";
+
+		System.out.println(reorder(INPUT, DISTANCE));
 	}
 }
 
@@ -59,7 +94,29 @@ class ValueComparator implements Comparator<Entry<Character, Integer>> {
 	public int compare(Entry<Character, Integer> o1,
 			Entry<Character, Integer> o2) {
 
-		return o1.getValue().compareTo(o2.getValue());
+		return o2.getValue().compareTo(o1.getValue());
+	}
+}
+
+class FixedSizeQueue<T> extends LinkedList<T> {
+
+	private static final long serialVersionUID = 1L;
+
+	private int maxSize;
+
+	public FixedSizeQueue(int size) {
+
+		super();
+
+		this.maxSize = size;
 	}
 
+	public void addLast(T t) {
+
+		while (super.size() >= maxSize) {
+			super.removeFirst();
+		}
+
+		super.addLast(t);
+	}
 }
