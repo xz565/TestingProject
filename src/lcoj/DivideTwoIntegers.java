1f0b32420e521214f772727b891f89dc8fc4541b
Xiaodong <zhang6078349@163.com>
2013-09-26 15:55:49 -0400

    accepted by OJ

--- src/lcoj/DivideTwoIntegers.java
+++ src/lcoj/DivideTwoIntegers.java
@@ -2,28 +2,35 @@ package lcoj;
 
 public class DivideTwoIntegers {
 
-	public static int solution(int dividend, int divisor) {
-		
-		if(divisor == 0)
+	public static int divide(int dividend, int divisor) {
+
+		long dividendAbs = Math.abs((long) dividend);
+		long divisorAbs = Math.abs((long) divisor);
+
+		if (divisorAbs == 0)
 			return -1;
-		
-		if(dividend == 0 || dividend < divisor)
+
+		if (dividendAbs == 0 || dividendAbs < divisorAbs)
 			return 0;
-		
-		int b2 = divisor;
-		int s = 1;
-		
-		while(dividend >= b2) {
-			b2 = b2 << 1;
-			s *= 2;
+
+		long divisor2 = divisorAbs;
+		long quotient = 1;
+
+		while (dividendAbs >= divisor2) {
+			divisor2 = divisor2 << 1;
+			quotient = quotient << 1;
 		}
-		s /= 2;
-		s += solution(dividend - (b2 >> 1), divisor);
-		
-		return s;
+		quotient = quotient >> 1;
+		quotient += divide((int) (dividendAbs - (divisor2 >> 1)),
+				(int) divisorAbs);
+
+		if ((dividend > 0 && divisor < 0) || (dividend < 0 && divisor > 0))
+			return (int) (0L - quotient);
+
+		return (int) quotient;
 	}
-	
+
 	public static void main(String[] args) {
-		System.out.println(solution(23, 4));
+		System.out.println(divide(2147483647, 1));
 	}
 }
