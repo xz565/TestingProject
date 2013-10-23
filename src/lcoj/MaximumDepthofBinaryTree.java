aa506be5a9ddb965bd7038cb585f741e0ffb5aaa
Xiaodong <zhang6078349@163.com>
2013-10-23 16:09:01 -0400

    solved Path Sum problem; made TreeNode as a public class

--- src/lcoj/MaximumDepthofBinaryTree.java
+++ src/lcoj/MaximumDepthofBinaryTree.java
@@ -12,10 +12,3 @@ public class MaximumDepthofBinaryTree {
     }
     
 }
-
-class TreeNode {
-     int val;
-     TreeNode left;
-     TreeNode right;
-     TreeNode(int x) { val = x; }
-}
---
+++ src/lcoj/PathSum.java
@@ -0,0 +1,33 @@
+package lcoj;
+
+
+/*
+ * Given a binary tree and a sum, determine if the tree has a root-to-leaf path such that 
+ * adding up all the values along the path equals the given sum.
+ */
+public class PathSum {
+
+  public static boolean hasPathSum(TreeNode root, int sum) {
+    
+    if(root == null) {
+      return false;
+    }
+    
+    int leftSum = sum - root.val;
+    
+    if(leftSum == 0 && root.left == null && root.right == null) {
+      return true;
+    }
+    
+    boolean left = hasPathSum(root.left, leftSum);
+    boolean right = hasPathSum(root.right, leftSum);
+
+    return left || right;
+  }
+  
+  public static void main(String[] args) {
+
+    TreeNode root = new TreeNode(1);
+    System.out.println(hasPathSum(root, 1));
+  }
+}
