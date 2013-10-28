16c35e5e3fef2ac806060fdda67105728ad63084
Xiaodong <zhang6078349@163.com>
2013-10-28 17:51:24 -0400

    solved Remove Nth Node From End of List problem

---
+++ src/lcoj/ListNode.java
@@ -0,0 +1,15 @@
+package lcoj;
+
+
+public class ListNode {
+
+  int val;
+  ListNode next;
+
+
+  ListNode(int x) {
+
+    val = x;
+    next = null;
+  }
+}
---
+++ src/lcoj/RemoveNthNodeFromEndOfList.java
@@ -0,0 +1,43 @@
+package lcoj;
+
+/*
+ * Given a linked list, remove the nth node
+ *  from the end of list and return its head. 
+ *  
+ *  Note:
+ *  Given n will always be valid.
+ *  Try to do this in one pass.
+ */
+
+public class RemoveNthNodeFromEndOfList {
+
+  public ListNode removeNthFromEnd(ListNode head, int n) {
+
+    ListNode root = new ListNode(0);
+    root.next = head;
+    ListNode faster = root;
+    
+    // move faster pointer n nodes ahead
+    for (int i = 0 ; i < n; i++) {
+      faster = faster.next;
+    }
+    
+    // if n equals the length of the list
+    if(faster.next == null) {
+      return head.next;
+    }
+    
+    ListNode slower = root;
+    // if n is greater than the length of the list
+    // move both pointer until faster reaches the end
+    while(faster.next != null) {
+      faster = faster.next;
+      slower = slower.next;
+    }
+    
+    // delete Nth node
+    slower.next = slower.next.next;
+    
+    return head;
+  }
+}
