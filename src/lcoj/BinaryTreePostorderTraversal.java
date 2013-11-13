4b54b6db07781b85f904b50b21052661012cb66b
Xiaodong <zhang6078349@163.com>
2013-11-13 10:58:58 -0500

    solved Binary TreePostorder Traversal problem

---
+++ src/lcoj/BinaryTreePostorderTraversal.java
@@ -0,0 +1,58 @@
+package lcoj;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Stack;
+
+
+public class BinaryTreePostorderTraversal {
+
+  public static ArrayList<Integer> postorderTraversal(TreeNode root) {
+
+    ArrayList<Integer> list = new ArrayList<Integer>();
+
+    Stack<TreeNode> stack = new Stack<TreeNode>();
+    Set<TreeNode> visited = new HashSet<TreeNode>();
+    stack.push(root);
+
+    while (!stack.isEmpty()) {
+
+      TreeNode currtNode = stack.peek();
+
+      if (currtNode != null) {
+
+        boolean flag = true;
+
+        if (currtNode.right != null && !visited.contains(currtNode.right)) {
+          flag = false;
+          stack.push(currtNode.right);
+        }
+
+        if (currtNode.left != null && !visited.contains(currtNode.left)) {
+          flag = false;
+          stack.push(currtNode.left);
+        }
+
+        if (flag) {
+          TreeNode cNode = stack.pop();
+          list.add(cNode.val);
+          visited.add(cNode);
+        }
+
+      } else {
+        stack.pop();
+      }
+
+    }
+
+    return list;
+  }
+
+
+  public static void main(String[] args) {
+
+    System.out.println(postorderTraversal(null));
+  }
+
+}
