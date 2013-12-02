2fd02d6f0608f1e35393191b6d5b822655c1941a
Xiaodong <zhang6078349@163.com>
2013-12-02 16:54:30 -0500

    another easy one

---
+++ src/lcoj/SameTree.java
@@ -0,0 +1,23 @@
+package lcoj;
+
+import lcoj.common.TreeNode;
+
+public class SameTree {
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+
+	if (p == null && q == null) {
+	    return true;
+	}
+
+	if (p != null && q != null && p.val == q.val) {
+	    return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+	}
+
+	return false;
+    }
+
+    public static void main(String[] args) {
+	System.out.println(null == null);
+    }
+}
