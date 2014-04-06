ce27d864f99c01ed0baed91bc7abae682f0bb3c9
xiaodong <zhang6078349@163.com>
2014-04-06 14:51:56 -0400

    solved one more

--- src/lcoj/common/TreeNode.java
+++ src/lcoj/common/TreeNode.java
@@ -2,12 +2,17 @@ package lcoj.common;
 
 public class TreeNode {
 
-    public int val;
-    public TreeNode left;
-    public TreeNode right;
+	public int val;
+	public TreeNode left;
+	public TreeNode right;
 
-    public TreeNode(int x) {
+	public TreeNode(int x) {
+		val = x;
+	}
 
-	val = x;
-    }
+	@Override
+	public String toString() {
+		return "TreeNode [val=" + val + ", left=" + left + ", right=" + right
+				+ "]";
+	}
 }
