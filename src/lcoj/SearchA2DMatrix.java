2ddcf0d36ed8fbc79deeb7e4bd5fd689651ba50d
xiaodong <zhang6078349@163.com>
2014-04-21 19:37:43 -0400

    binary search

--- src/lcoj/SearchA2DMatrix.java
+++ src/lcoj/SearchA2DMatrix.java
@@ -4,12 +4,12 @@ package lcoj;
  * Write an efficient algorithm that searches for a value in an m x n matrix. This matrix has the following properties:
  * 
  * Integers in each row are sorted from left to right. 
- * The first integer of each row is greater than the last integer of the previous row. For example,
+ * The first integer of each row is greater than the LAST integer of the previous row. For example,
  * 
  * Consider the following matrix:
  * 
- * [ [1, 3, 5, 100],
- * [10, 11, 200, 300], 
+ * [ [1, 3, 5, 7],
+ * [8, 11, 15, 20], 
  * [23, 30, 34, 50] ] 
  * 
  * Given target = 3, return true.
@@ -19,15 +19,38 @@ package lcoj;
  */
 public class SearchA2DMatrix {
 
-  // tricky question, Yong's matrix
-  // search from lower left corner to determine the row
-  // then within a row, use binary search
-  // overall time complexity is O(N) + O(lgM)
-  
-  // compare to binary search in each row which is O(NlgM)
+  // binary search
+  // log (m+n)
   public boolean searchMatrix(int[][] matrix, int target) {
 
+    int head = 0;
+    int tail = matrix.length*matrix[0].length - 1;
+    int colm = matrix[0].length;
+    
+    while(head <= tail) {
+    	
+    	int mid = head + (tail - head) / 2;
+    	int row = mid / colm;
+    	int col = mid % colm;
+
+    	if(matrix[row][col] == target) {
+    		return true;
+    	}
+    	if(matrix[row][col] < target) {
+    		head = mid + 1; // !!! important
+    	} else {
+    		tail = mid - 1; // !!! important
+    	}
+    	
+    }
     
     return false;
+  }
+  
+  public static void main(String[] args) {
+	
+	  int[][] matrix = {{1, 3}};
+	  SearchA2DMatrix searchA2DMatrix = new SearchA2DMatrix();
+	  System.out.println(searchA2DMatrix.searchMatrix(matrix, 7));
   }
 }
