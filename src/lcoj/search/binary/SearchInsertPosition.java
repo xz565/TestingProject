3ad0a53b7dc25838836df355ed7c7c95dbc8c84c
xiaodong <zhang6078349@163.com>
2014-04-21 19:50:35 -0400

    adjusted packages

---
+++ src/lcoj/search/binary/SearchInsertPosition.java
@@ -0,0 +1,25 @@
+package lcoj.search.binary;
+
+//Given a sorted array and a target value, return the index if the target is found. 
+//If not, return the index where it would be if it were inserted in order.
+//
+//You may assume no duplicates in the array.
+//
+//Here are few examples.
+//[1,3,5,6], 5 → 2
+//[1,3,5,6], 2 → 1
+//[1,3,5,6], 7 → 4
+//[1,3,5,6], 0 → 0
+public class SearchInsertPosition {
+
+	// O(N) linear solution
+	// better to find a logN solution
+    public int searchInsertBad(int[] A, int target) {
+    	for(int i = 0; i < A.length; i++) {
+    		if(target <= A[i]) {
+    			return i;
+    		}
+    	}
+    	return A.length;
+    }
+}
