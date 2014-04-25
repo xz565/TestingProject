42419f4fb28e566eca9f18b83da2d574fb748095
Xiaodong <zhang6078349@163.com>
2014-04-24 22:24:03 -0400

    DELL local

---
+++ src/lcoj/search/binary/SearchInRotatedSortedArray.java
@@ -0,0 +1,24 @@
+package lcoj.search.binary;
+
+// Suppose a sorted array is rotated at some pivot unknown to you beforehand.
+//
+// (i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
+//
+// You are given a target value to search. If found in the array return its index, otherwise return -1.
+//
+// You may assume no duplicate exists in the array.
+public class SearchInRotatedSortedArray {
+
+  public int search(int[] A, int target) {
+
+    return 0;
+  }
+
+
+  public static void main(String[] args) {
+
+    SearchInRotatedSortedArray searchInRotatedSortedArray = new SearchInRotatedSortedArray();
+    int[] A = {};
+    System.out.println(searchInRotatedSortedArray.search(A, 1));
+  }
+}
