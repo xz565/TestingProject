e1672532617a42b25155ec5df24f1a09f2c3d014
xiaodong <zhang6078349@163.com>
2014-04-27 22:40:03 -0400

    used binary search to solve an old problem

--- src/lcoj/search/binary/SearchInsertPosition.java
+++ src/lcoj/search/binary/SearchInsertPosition.java
@@ -14,6 +14,7 @@ public class SearchInsertPosition {
 
 	// O(N) linear solution
 	// better to find a logN solution
+	// Got AC by OJ...
     public int searchInsertBad(int[] A, int target) {
     	for(int i = 0; i < A.length; i++) {
     		if(target <= A[i]) {
@@ -22,4 +23,38 @@ public class SearchInsertPosition {
     	}
     	return A.length;
     }
+    
+    
+    // a binary search solution
+    // A variation of regular binary search
+    public int searchInsert(int[] A, int target) {
+        
+    	int head = 0;
+    	int tail = A.length - 1;
+    	int mid = 0;
+    	
+    	while(head <= tail) {
+    		mid = head + (tail - head) / 2;
+    		if(target > A[mid]) {
+    			head = mid + 1;
+    		} else if (target < A[mid]) {
+    			tail = mid - 1;
+    		} else {
+    			return mid;
+    		}
+    	}
+    	
+    	if(target > A[mid]) {
+    		return mid + 1;
+    	} else {
+    		return mid;
+    	}
+    }
+    
+    public static void main(String[] args) {
+		
+    	int[] A = {1,3,5,6};
+    	SearchInsertPosition searchInsertPosition = new SearchInsertPosition();
+    	System.out.println(searchInsertPosition.searchInsert(A, 0));
+	}
 }
