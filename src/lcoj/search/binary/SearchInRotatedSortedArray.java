af5de098f497c96770a1ad65375f9d7a50f97d2d
xiaodong <zhang6078349@163.com>
2014-04-28 08:20:23 -0400

    IBM local

--- src/lcoj/search/binary/SearchInRotatedSortedArray.java
+++ src/lcoj/search/binary/SearchInRotatedSortedArray.java
@@ -1,5 +1,7 @@
 package lcoj.search.binary;
 
+import com.sun.accessibility.internal.resources.accessibility;
+
 // Suppose a sorted array is rotated at some pivot unknown to you beforehand.
 //
 // (i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
@@ -9,16 +11,68 @@ package lcoj.search.binary;
 // You may assume no duplicate exists in the array.
 public class SearchInRotatedSortedArray {
 
-  public int search(int[] A, int target) {
+  // wrong thought, good to keep
+  public int search_wrong(int[] A, int target) {
 
-    return 0;
+	  int head = 0;
+	  int tail = A.length - 1;
+	  int mid;
+	  
+	  while(head <= tail) {
+		  mid = (head + tail) /2;
+		  if(target < A[mid]) {
+			  if(mid+1 < A.length && A[mid + 1] < A[mid] && A[tail] >= target) {
+				  head = mid + 1;
+			  } else {
+				  tail = mid - 1;
+			  }
+		  } else if(target > A[mid]) {
+			  if(mid-1 >=0 && A[mid - 1] > A[mid] && A[head] <= target) {
+				  tail = mid - 1;
+			  } else {
+				  head = mid + 1;
+			  }
+		  } else {
+			  return mid;
+		  }
+	  }
+	  
+    return -1;
+  }
+  
+  public int search(int[] A, int target) {
+	  
+	  int head = 0;
+	  int tail = A.length - 1;
+	  int mid = 0;
+	  
+	  while(head <= tail) {
+		  mid = head + (tail - head) / 2;
+		  if(target < A[mid]) {
+			  if(target >= A[head]) {
+				  tail = mid - 1;
+			  } else {
+				  head = mid + 1;
+			  }
+		  } else if(target > A[mid]) {
+			  if(target <= A[tail]) {
+				  head = mid + 1;
+			  } else {
+				  tail = mid - 1;
+			  }
+		  } else {
+			  return mid;
+		  }
+	  }
+	  
+	  return -1;
   }
 
 
   public static void main(String[] args) {
 
     SearchInRotatedSortedArray searchInRotatedSortedArray = new SearchInRotatedSortedArray();
-    int[] A = {};
-    System.out.println(searchInRotatedSortedArray.search(A, 1));
+    int[] A = {3,4,5,6,1,2};
+    System.out.println(searchInRotatedSortedArray.search(A, 3));
   }
 }
