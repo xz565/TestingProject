28ca034fe33359f829be5e973eaaa57611120ac3
Xiaodong <zhang6078349@163.com>
2014-04-30 23:08:58 -0400

    local DELL

---
+++ src/lcoj/array/sum/FourSum.java
@@ -0,0 +1,28 @@
+package lcoj.array.sum;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+// Given an array S of n integers, are there elements a, b, c, and d in S such that a + b + c + d = target?
+// Find all unique quadruplets in the array which gives the sum of target.
+//
+// Note:
+// Elements in a quadruplet (a,b,c,d) must be in non-descending order. (ie, a ≤ b ≤ c ≤ d)
+// The solution set must not contain duplicate quadruplets.
+// For example, given array S = {1 0 -1 0 -2 2}, and target = 0.
+//
+// A solution set is:
+// (-1, 0, 0, 1)
+// (-2, -1, 1, 2)
+// (-2, 0, 0, 2)
+public class FourSum {
+
+  public ArrayList<ArrayList<Integer>> fourSum(int[] num, int target) {
+
+    Arrays.sort(num);
+    ArrayList<ArrayList<Integer>> results = new ArrayList<ArrayList<Integer>>();
+
+
+    return results;
+  }
+}
