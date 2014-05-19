ee1bc96ca66eb8ef259897e2f0b85fa3940a0257
Xiaodong <zhang6078349@163.com>
2014-05-19 16:39:16 -0400

    solved path sum II

--- src/lcoj/tree/PathSum.java
+++ src/lcoj/tree/PathSum.java
@@ -4,29 +4,27 @@ import lcoj.common.TreeNode;
 
 
 /*
- * Given a binary tree and a sum, determine if the tree has a root-to-leaf path such that 
- * adding up all the values along the path equals the given sum.
+ * Given a binary tree and a sum, determine if the tree has a root-to-leaf path such that adding up all the values along the path equals the given sum.
  */
 public class PathSum {
 
   public static boolean hasPathSum(TreeNode root, int sum) {
-    
-    if(root == null) {
+
+    if (root == null) {
       return false;
     }
-    
+
     int leftSum = sum - root.val;
-    
-    if(leftSum == 0 && root.left == null && root.right == null) {
+
+    // find the leave node whose path adds to sum
+    if (leftSum == 0 && root.left == null && root.right == null) {
       return true;
     }
-    
-    boolean left = hasPathSum(root.left, leftSum);
-    boolean right = hasPathSum(root.right, leftSum);
 
-    return left || right;
+    return hasPathSum(root.left, leftSum) || hasPathSum(root.right, leftSum);
   }
-  
+
+
   public static void main(String[] args) {
 
     TreeNode root = new TreeNode(1);
