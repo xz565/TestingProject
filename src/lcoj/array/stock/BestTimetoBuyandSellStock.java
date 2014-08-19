441ab413e0d4991ed82d7b629de5fca7831815cb
Xiaodong <zhang6078349@163.com>
2014-08-18 21:20:11 -0400

    stock stock

--- src/lcoj/array/stock/BestTimetoBuyandSellStock.java
+++ src/lcoj/array/stock/BestTimetoBuyandSellStock.java
@@ -1,48 +1,85 @@
 package lcoj.array.stock;
 
+// Say you have an array for which the ith element is the price of a given stock on day i.
+//
+// If you were only permitted to complete at most one transaction
+// (ie, buy one and sell one share of the stock),
+// design an algorithm to find the maximum profit.
 public class BestTimetoBuyandSellStock {
 
-	// O(N^2) TLE
-    public int maxProfitBad(int[] prices) {
-     
-    	int max = 0;
-    	for(int i = 0; i < prices.length; i++) {
-    		for( int j = i + 1; j < prices.length; j++) {
-    			int diff = prices[j] - prices[i];
-    			if(diff > max) {
-    				max = diff;
-    			}
-    		}
-    	}
-    	return max;
+  // O(N^2) TLE
+  public int maxProfitBad(int[] prices) {
+
+    int max = 0;
+    for (int i = 0 ; i < prices.length ; i++) {
+      for (int j = i + 1 ; j < prices.length ; j++) {
+        int diff = prices[j] - prices[i];
+        if (diff > max) {
+          max = diff;
+        }
+      }
     }
-    
-    // O(N) time
-    public int maxProfit(int[] prices) {
-        
-    	int max = 0;
-    	int currtMax = 0;
-    	
-    	for(int i = 1; i < prices.length; i++) {
-    		int diff = prices[i] - prices[i-1];
-    		if((diff + currtMax) < 0) {
-    			currtMax = 0;
-    		} else {
-    			currtMax += diff;
-    		}
-    		
-    		if(currtMax > max) {
-    			max = currtMax;
-    		}
-    	}
-    	
-    	return max;
+    return max;
+  }
+
+
+  // O(N) time
+  public int maxProfit(int[] prices) {
+
+    int max = 0;
+    int currtMax = 0;
+
+    for (int i = 1 ; i < prices.length ; i++) {
+      int diff = prices[i] - prices[i - 1];
+      if ((diff + currtMax) < 0) {
+        currtMax = 0;
+      } else {
+        currtMax += diff;
+      }
+
+      if (currtMax > max) {
+        max = currtMax;
+      }
     }
-    
-    public static void main(String[] args) {
-		int[] prices = {1,4,2};
-		
-		BestTimetoBuyandSellStock bt = new BestTimetoBuyandSellStock();
-		System.out.println(bt.maxProfit(prices));
-	}
+
+    return max;
+  }
+
+
+  // dp[i] is the max profit between [0,i]
+  // max profit at a point i is prices[i] - minPrice
+  // so the recursion is: dp[i+1] = max(dp[i], prices[i+1] - minPrice)
+  // where minPrice is the min price between [0,i]
+  //
+  // This one is easier to understand than the previous one, which is too tricky
+  public int maxProfit_DP(int[] prices) {
+
+    if (prices.length == 0) {
+      return 0;
+    }
+
+    int[] dp = new int[prices.length];
+
+    int max = 0;
+    int minPrice = prices[0];
+    for (int i = 1 ; i < prices.length ; i++) {
+      dp[i] = Math.max(dp[i - 1], prices[i] - minPrice);
+      minPrice = Math.min(minPrice, prices[i]);
+
+      if (dp[i] > max) {
+        max = dp[i];
+      }
+    }
+
+    return max;
+  }
+
+
+  public static void main(String[] args) {
+
+    int[] prices = { 1, 4, 2 };
+
+    BestTimetoBuyandSellStock bt = new BestTimetoBuyandSellStock();
+    System.out.println(bt.maxProfit(prices));
+  }
 }
---
+++ src/lcoj/array/stock/BestTimetoBuyandSellStockIII.java
@@ -0,0 +1,46 @@
+package lcoj.array.stock;
+
+// Say you have an array for which the ith element is the price of a given stock on day i.
+//
+// Design an algorithm to find the maximum profit.
+// You may complete at most two transactions.
+//
+// Note:
+// You may not engage in multiple transactions at the same time
+// (ie, you must sell the stock before you buy again).
+public class BestTimetoBuyandSellStockIII {
+
+  // from head to tail
+  // dp[i+1] = max(dp[i], prices[i+1] - minPrice)
+  // from tail to head
+  // dp[i-1] = max(dp[i], maxPrice - prices[i-1]);
+  public int maxProfit(int[] prices) {
+
+    if (prices.length <= 1) {
+      return 0;
+    }
+
+    // left to right
+    int[] maxFromHead = new int[prices.length];
+    int minPrice = prices[0];
+    for (int i = 1 ; i < prices.length ; i++) {
+      maxFromHead[i] = Math.max(maxFromHead[i - 1], prices[i] - minPrice);
+      minPrice = Math.min(minPrice, prices[i]);
+    }
+
+    // right to left
+    int result = maxFromHead[prices.length - 1];
+    int curtMax = 0;
+    int maxPrice = prices[prices.length - 1];
+    for (int i = prices.length - 2 ; i >= 0 ; i--) {
+      maxPrice = Math.max(maxPrice, prices[i + 1]);
+      curtMax = Math.max(curtMax, maxPrice - prices[i]);
+      if (curtMax + maxFromHead[i] > result) {
+        result = curtMax + maxFromHead[i];
+      }
+
+    }
+
+    return result;
+  }
+}
