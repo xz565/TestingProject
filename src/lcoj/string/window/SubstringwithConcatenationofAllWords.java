99532bb291816619fcd3957ca09b51b82f62e63d
Xiaodong <zhang6078349@163.com>
2014-08-24 20:35:23 -0400

    BF one, need to optimize

--- src/lcoj/string/window/SubstringwithConcatenationofAllWords.java
+++ src/lcoj/string/window/SubstringwithConcatenationofAllWords.java
@@ -20,7 +20,9 @@ public class SubstringwithConcatenationofAllWords {
 
   // similar to min window substring
   // we maintain a sliding window to reduce the time complexity to linear
-  public List<Integer> findSubstring(String S, String[] L) {
+
+  // This is actually O(N^2) solution
+  public static List<Integer> findSubstring_BAD(String S, String[] L) {
 
     List<Integer> rst = new ArrayList<Integer>();
     if (S == null || L == null || L.length == 0) {
@@ -36,14 +38,53 @@ public class SubstringwithConcatenationofAllWords {
       LMap.put(str, LMap.get(str) + 1);
     }
 
+
     int head = 0;
     while (S.length() - head >= LEN * L.length) {
-      // Map<String, Integer> tempMap = new HashMap<String, Integer>(LMap);
 
+      String headTemp = S.substring(head, head + LEN);
 
+      System.out.println(LMap);
+      System.out.println(headTemp);
+
+      if (LMap.containsKey(headTemp)) {
+        Map<String, Integer> tempMap = new HashMap<String, Integer>(LMap);
+
+        int tail = head;
+        while (tail < S.length()) {
+
+          // System.out.println(tempMap);
+
+          String temp = S.substring(tail, tail + LEN);
+
+          if (tempMap.containsKey(temp) && tempMap.get(temp) == 1) {
+            tempMap.remove(temp);
+          } else if (!tempMap.containsKey(temp)) {
+            break;
+          } else {
+            tempMap.put(temp, tempMap.get(temp) - 1);
+          }
+
+          if (tempMap.isEmpty()) {
+            rst.add(head);
+            break;
+          }
+
+          tail += LEN;
+        }
+      }
+
+      head++;
     }
 
     return rst;
+  }
+
+
+  // looks like the slidig window solution is complex
+  public static List<Integer> findSubstring(String S, String[] L) {
+
+    return null;
   }
 
 
@@ -51,8 +92,10 @@ public class SubstringwithConcatenationofAllWords {
 
     String S = "barfoothefoobarman";
     String[] L = { "foo", "bar" };
+    System.out.println(findSubstring(S, L));
 
-    SubstringwithConcatenationofAllWords substringwithConcatenationofAllWords = new SubstringwithConcatenationofAllWords();
-    System.out.println(substringwithConcatenationofAllWords.findSubstring(S, L));
+    S = "lingmindraboofooowingdingbarrwingmonkeypoundcake";
+    String[] L1 = { "fooo", "barr", "wing", "ding", "wing" };
+    System.out.println(findSubstring(S, L1));
   }
 }
