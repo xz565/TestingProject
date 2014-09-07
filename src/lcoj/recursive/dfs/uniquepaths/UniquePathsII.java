ea9ade1ca51b3d3ea9ccd6c52ce19c6edd76296c
xiaodong <zhang6078349@163.com>
2014-09-07 18:15:04 -0400

    very interesting fact about boundary check

--- src/lcoj/recursive/dfs/uniquepaths/UniquePathsII.java
+++ src/lcoj/recursive/dfs/uniquepaths/UniquePathsII.java
@@ -17,6 +17,9 @@ package lcoj.recursive.dfs.uniquepaths;
 //The total number of unique paths is 2.
 //
 //Note: m and n will be at most 100.
+
+// This is a typical DFS + DP problem
+// easy in algorithm, problem is how to write it neat and bug free
 public class UniquePathsII {
 
 	public int uniquePathsWithObstacles(int[][] obstacleGrid) {
@@ -72,11 +75,54 @@ public class UniquePathsII {
 		}
 	}
 
+	
+	// Interesting fact
+	// First tried from [0,0] to lower right corner,
+	// however, got TLE because of boundary check !!!
+	//
+	// then reversed from [obstacleGrid-1][obstacleGrid[0].length] to [0, 0], and got accepted !!!
+	// looks like the .length operation is expensive in Java OR because the recursive step is too much
+	int[][] cache;
+	public int uniquePathsWithObstacles2(int[][] obstacleGrid) {
+		
+		if(obstacleGrid == null || obstacleGrid.length == 0 || obstacleGrid[0].length == 0) {
+			return 0;
+		}
+		
+		cache = new int[obstacleGrid.length][obstacleGrid[0].length];
+		int rst = helper(obstacleGrid, obstacleGrid.length - 1, obstacleGrid[0].length - 1);
+	
+		return rst;
+	}
+	
+	
+	private int helper(int[][] obstacleGrid, int row, int col) {
+
+		if(row < 0 || col < 0) {
+			return 0;
+		}
+		
+		if(cache[row][col] == 0) {
+			
+			if(obstacleGrid[row][col] == 1) {
+			    cache[row][col] = -1;
+			} else {
+			    if(row == 0 && col == 0) {
+			        cache[row][col] = 1;
+			    } else {
+			        cache[row][col] = helper(obstacleGrid, row - 1, col) + helper(obstacleGrid, row, col - 1);
+			    }
+			}
+		}
+		
+		return cache[row][col] == -1 ? 0 : cache[row][col];
+	}
+
 	public static void main(String[] args) {
 		int[][] grid = { { 0, 0, 0 }, { 0, 1, 0 }, { 0, 0, 0 } };
 		int[][] grid2 = { { 1, 0 } };
 		UniquePathsII uniquePathsII = new UniquePathsII();
-		System.out.println(uniquePathsII.uniquePathsWithObstacles(grid));
-		System.out.println(uniquePathsII.uniquePathsWithObstacles(grid2));
+		System.out.println(uniquePathsII.uniquePathsWithObstacles2(grid));
+		System.out.println(uniquePathsII.uniquePathsWithObstacles2(grid2));
 	}
 }
