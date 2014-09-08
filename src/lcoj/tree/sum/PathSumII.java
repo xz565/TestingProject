f18f0cd67298464ab99d5bea8ffef58cc1876474
xiaodong <zhang6078349@163.com>
2014-09-08 07:51:47 -0400

    You never know what kind of mistake you will make until you actually write it youself

--- src/lcoj/tree/sum/PathSumII.java
+++ src/lcoj/tree/sum/PathSumII.java
@@ -2,24 +2,25 @@ package lcoj.tree.sum;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 
 import lcoj.common.TreeNode;
 
 // Given a binary tree and a sum, find all root-to-leaf paths where each path's sum equals the given sum.
-//
+// 
 // For example:
 // Given the below binary tree and sum = 22,
-// 5
-// / \
-// 4 8
-// / / \
-// 11 13 4
-// / \ / \
-// 7 2 5 1
+//               5
+//              / \
+//             4   8
+//            /   / \
+//           11  13  4
+//          /  \    / \
+//         7    2  5   1
 // return
 // [
-// [5,4,11,2],
-// [5,8,4,5]
+//    [5,4,11,2],
+//    [5,8,4,5]
 // ]
 public class PathSumII {
 
@@ -61,6 +62,40 @@ public class PathSumII {
   }
 
 
+  // Pitfall: to judge a node is leaf, both its children should be null
+  // can not add the list when we meet a null node because this will duplicate the result
+  public List<List<Integer>> pathSum2(TreeNode root, int sum) {
+      
+      List<List<Integer>> rst = new ArrayList<List<Integer>>();
+      if(root == null) {
+          return rst;
+      }
+      
+      List<Integer> tmpPath = new ArrayList<Integer>();
+      dfsHelper(root, sum, rst, tmpPath);
+      
+      return rst;
+  }
+  
+  void dfsHelper(TreeNode root, int sum, List<List<Integer>> rst, List<Integer> tmpPath) {
+      if(root == null) {
+          return;
+      }
+      
+      if(root.val == sum && root.left == null && root.right == null) {
+          tmpPath.add(root.val);
+          rst.add(new ArrayList<Integer>(tmpPath));
+          tmpPath.remove(tmpPath.size() - 1);
+          return;
+      }
+      
+      tmpPath.add(root.val);
+      dfsHelper(root.left, sum - root.val, rst, tmpPath);
+      dfsHelper(root.right, sum - root.val, rst, tmpPath);
+      tmpPath.remove(tmpPath.size() - 1);
+  }
+  
+  
   public static void main(String[] args) {
 
     TreeNode root = new TreeNode(1);
